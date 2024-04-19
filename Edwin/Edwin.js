cca3079d586d7b6fef6e27ced608c8c888c9aab3
Edwin.C <144722601+Edwinc123@users.noreply.github.com>
2024-04-19 13:36:48 -0400

    Update Edwin.js

--- Edwin/Edwin.js
+++ Edwin/Edwin.js
@@ -9,3 +9,17 @@ window.addEventListener('load', () => {
     document.body.style.overflow = 'inital';
   },1000);
 });
+
+const coinSfx = new sfx('sfx/Mario Coin.wav');
+const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
+const powerUpSfx = new Sfx('sfx/Mario Powerup.wav');
+const container = document.querySelector('.content');
+let ninja;
+
+function Sfx(src) {
+  this.sound = document.createElement('audio');
+  
+
+
+
+
