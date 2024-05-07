435531606f0589d18a044f67e90a6db9c48cf6e0
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-05-07 12:30:15 -0400

    Update deveshS.js

--- Devesh/deveshS.js
+++ Devesh/deveshS.js
@@ -1,7 +1,7 @@
 const coinSfx = new Sfx('sfx/Mario Coin.wav');
 const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
 const powerUpSfx = new Sfx ('sfx/Mario Powerup.wav');
-const container = document.querySlector('.content');
+const container = document.querySelector('.content');
 let ninja; 
 
 function Sfx(src) {
